85472b2e39b44732a9abe275e3d8c340e71a86be
Abdüsselam Demirkol <wdemirkoll.dev@gmail.com>
2026-04-16 11:57:07 +0300

    fix: add missing User include in Menus header

--- include/Menus.h
+++ include/Menus.h
@@ -1,6 +1,9 @@
 #ifndef MENUS_H
 #define MENUS_H
 
+#include "Bank.h"
+#include "User.h"
+
 // Menu functions displaying statistics and the main menu.
 void showStats(const Bank& bank, const User& user);
 void showMainMenu();
